7660cc0acee814b940b37d9c49e5d7533c128a88
sesamechicken <chrisdickow@gmail.com>
2017-07-31 11:43:23 -0400

    Update dotfiles.sh

--- dotfiles.sh
+++ dotfiles.sh
@@ -7,63 +7,32 @@ export ANDROID_HOME=/Users/chris/dev/android-sdk-macosx
 export PATH=${PATH}:$ANDROID_HOME/tools:$ANDROID_HOME/platform-tools
 
 export PATH="/usr/local/bin:$PATH"
+if [ -f $(brew --prefix)/etc/bash_completion ]; then
+    . $(brew --prefix)/etc/bash_completion
+  fi
+export NVM_DIR="$HOME/.nvm"
+  . "/usr/local/opt/nvm/nvm.sh"
 
-
-source ~/.git-completion.bash
-source ~/.git-prompt.sh
-
-
-GIT_PS1_SHOWDIRTYSTATE=true
-export PS1='[\u@\h \w$(__git_ps1)]\$ '
-
-#Start custom stuff
-
-# Detect which `ls` flavor is in use
-if ls --color > /dev/null 2>&1; then # GNU `ls`
-	colorflag="--color"
-else # OS X `ls`
-	colorflag="-G"
+if [ -f ~/.git-completion.bash ]; then
+  . ~/.git-completion.bash
 fi
-
-# git stuff
-
-if [ -f ~/.git-completion.bash ];
-then
-
-. ~/.git-completion.bash
-
-fi
-
-export CLICOLOR=1
-
-export LSCOLORS=ExFxBxDxCxegedabagacad
+SB_GREEN="\[\033[1;32m\]"
+SB_BLUE="\[\033[1;34m\]"
+SB_RED="\[\033[1;31m\]"
+SB_NOCOLOR="\[\033[0m\]"
+export PS1="$SB_GREEN\u@:\W$SB_GREEN\$(parse_git_branch)$SB_RED $SB_NOCOLOR$"
 parse_git_branch() {
-
-git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/ (\1)/'
-
+  git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/ (\1)/'
 }
-
-PS1="[\[\033[36m\]\u\[\033[37m\]@\[\033[33;1m\]\w\[\033[m\]\[\033[32m\]\$(parse_git_branch)\[\033[m\]\]$ "
-
-
-
-# List all files colorized in long format
-alias l="ls -lF ${colorflag}"
-
-# Always use color output for `ls`
-alias ls="command ls ${colorflag}"
-export LS_COLORS='no=00:fi=00:di=01;34:ln=01;36:pi=40;33:so=01;35:do=01;35:bd=40;33;01:cd=40;33;01:or=40;31;01:ex=01;32:*.tar=01;31:*.tgz=01;31:*.arj=01;31:*.taz=01;31:*.lzh=01;31:*.zip=01;31:*.z=01;31:*.Z=01;31:*.gz=01;31:*.bz2=01;31:*.deb=01;31:*.rpm=01;31:*.jar=01;31:*.jpg=01;35:*.jpeg=01;35:*.gif=01;35:*.bmp=01;35:*.pbm=01;35:*.pgm=01;35:*.ppm=01;35:*.tga=01;35:*.xbm=01;35:*.xpm=01;35:*.tif=01;35:*.tiff=01;35:*.png=01;35:*.mov=01;35:*.mpg=01;35:*.mpeg=01;35:*.avi=01;35:*.fli=01;35:*.gl=01;35:*.dl=01;35:*.xcf=01;35:*.xwd=01;35:*.ogg=01;35:*.mp3=01;35:*.wav=01;35:'
-
 alias reload="source ~/.bash_profile"
-
 alias ..='cd ../'
-
 alias ...='cd ../../'
-
+alias cc='clear'
 alias code='open -a "Visual Studio Code"'
-
 alias chrome='open -a "Google Chrome"'
+alias ls='ls -la -G'
+alias loggy='git log --graph --decorate'
 
-alias atom='open -a "Atom"'
-
-alias subl='open -a "Sublime Text"'
+function update(){
+  git checkout master && git pull && git checkout - && git rebase master
+}
